f0e883135603bd1bb6fbff9ab92d2013ad65803c
Timo Hackel <timo.hackel@geod.baug.ethz.ch>
2017-02-17 16:12:20 +0100

    continued work

--- tensorflow/core/user_ops/sparse_tensor_dense_conv_3d.cc
+++ tensorflow/core/user_ops/sparse_tensor_dense_conv_3d.cc
@@ -1,76 +1,212 @@
 #include <map>
+#include <sstream>
 #include "tensorflow/core/framework/op.h"
 #include "tensorflow/core/framework/shape_inference.h"
+#include "tensorflow/core/framework/common_shape_fns.h"
+#include "sparse_tensor_sparse_kernel_dense_conv_3d.h"
 
+
+/** SparseTensorSparseKernelDenseConv3D
+  * \ingroup CXX11_NeuralNetworks_Module
+  * 
+  * \brief Applies a 3D convolution over a multichannel input voxel block.
+  * 
+  * The input parameter is expected to be a tensor with a rank of 4 or more (channels, depth, height, width, and optionally others).
+  * The kernel parameter is expected to be a 5D tensor (filters, channels, kernel_depth, kernel_height, kernel_width).
+  * 
+  * The result can be assigned to a tensor of rank equal to the rank of the input. The dimensions of the result will be filters, depth, height, width (and others if applicable).
+  */
+
+
+
+//TODO: How do I use REGISTER_OP with parameter T?
+//  .Attr("T: {float, double, int32, complex64, complex128}")
 REGISTER_OP("SparseTensorDenseConv3D")
-    .Input("input: T")
-    .Input("filter: T")
-    .Output("output: T")
-    .Attr("T: numbertype")
-    .Attr("strides: list(int) >= 5")
-    .Doc(R"doc(
-Computes a 3-D convolution given sparse 5-D `input` and  dense`filter` tensors.
-
-In signal processing, cross-correlation is a measure of similarity of
-two waveforms as a function of a time-lag applied to one of them. This
-is also known as a sliding dot product or sliding inner-product.
-
-Our Conv3D implements a form of cross-correlation.
-
-input: Shape `[batch, in_depth, in_height, in_width, in_channels]`.
-filter: Shape `[filter_depth, filter_height, filter_width, in_channels,
-  out_channels]`. `in_channels` must match between `input` and `filter`.
-strides: 1-D tensor of length 5. The stride of the sliding window for each
-  dimension of `input`. Must have `strides[0] = strides[4] = 1`.
-padding is automatically set to zero padding to keep the tensor sparse
-)doc");
+  .Attr("T: {int32}")
+  .Input("in_indices: int64")
+  .Input("in_values: T")
+  .Input("in_shape: int64")
+  .Input("dense_filter: T")
+  .Output("sparse_indices: int64")
+  .Output("sparse_values: T")
+  .Output("sparse_shape: int64")
+  .Attr("strides: list(int) >= 5");
 
 
 #include "tensorflow/core/framework/op_kernel.h"
 
+typedef Eigen::ThreadPoolDevice CPUDevice;
+
 using namespace tensorflow;
 
-class SparseTensorDenseConv3D : public OpKernel {
+template<typename T, int N>
+class Converter{
  public:
-  explicit SparseTensorDenseConv3D(OpKernelConstruction* context) : OpKernel(context) {}
+  void convert(const Tensor* t_dense, Tensor* s_ind, Tensor* s_val, const T&  epsilon) const { /*generic case not implemented*/};
+};
 
-  void Compute(OpKernelContext* context) override {
-    const Tensor& input_tensor = context->input(0);
-    const Tensor& filter = context->input(1);
-    auto input = input_tensor.flat<int32>();
-    std::vector<int32> stride;
-    //OP_REQUIRES_OK(context, context->GetAttr("strides", &stride));
-
-    /*
-    for(size_t i = 0; i < input_indices.size(); ++i){
-      if(index_is_unaffected_by_stride(input_indices[i], stride)){
-          update_indices, update_values = dense_filter_update_output(input_indices[i], input_values[i], filter);
-          SparseAddOp(update_indices, update_values)
-      }
-    }*/
-    
-
-
-  }
-
- private:  
-  /*inline bool index_is_unaffected_by_stride(const std::vector<int32>& id, const std::vector<int32>& stride) const {
-    assert(id.size() == stride.size());
-    for(int32 i = 0; i < stride.size(); ++i){
-      if(stride[i] > 0){
-        if(((id[i] + 1) % stride[i]) == 0){
-          return false;
+template<typename T>
+class Converter<T, 5>{
+ public:
+  void convert(const Tensor* t_dense, Tensor* s_ind, Tensor* s_val, const T&  epsilon) const {
+    auto out_ind = s_ind->matrix<int64>(); 
+    auto out_vals = s_val->flat<T>();
+    auto d_data = t_dense->tensor<T, 5>();
+    int64 id_cnt=0;
+    for(int64 i = 0;  i < d_data.dimension(0); ++i){
+      for(int64 j = 0;  j < d_data.dimension(1); ++j){
+        for(int64 k = 0;  k < d_data.dimension(2); ++k){
+          for(int64 l = 0;  l < d_data.dimension(3); ++l){
+            for(int64 m = 0;  m < d_data.dimension(4); ++m){
+              if(d_data(i,j,k,l,m) > epsilon || d_data(i,j,k,l,m) < epsilon){
+                id_cnt++;
+                out_ind(id_cnt, 0) = i; out_ind(id_cnt, 1) = j; out_ind(id_cnt, 2) = k; out_ind(id_cnt, 3) = l;
+                out_vals(id_cnt) = d_data(i,j,k,l,m);
+              }
+            }
+          }
         }
       }
     }
-    return true;
+  };
+};
+
+template<typename T>
+class Converter<T, 4>{
+ public:
+  void convert(const Tensor* t_dense, Tensor* s_ind, Tensor* s_val, const T&  epsilon) const {
+    auto out_ind = s_ind->matrix<int64>(); 
+    auto out_vals = s_val->flat<T>();
+    auto d_data = t_dense->tensor<T, 4>();
+    int64 id_cnt=0;
+    for(int64 i = 0;  i < d_data.dimension(0); ++i){
+     for(int64 j = 0;  j < d_data.dimension(1); ++j){
+        for(int64 k = 0;  k < d_data.dimension(2); ++k){
+          for(int64 l = 0;  l < d_data.dimension(3); ++l){
+
+            if(d_data(i,j,k,l) > epsilon || d_data(i,j,k,l) < epsilon){
+              id_cnt++;
+              out_ind(id_cnt, 0) = i; out_ind(id_cnt, 1) = j; out_ind(id_cnt, 2) = k; out_ind(id_cnt, 3) = l;
+              out_vals(id_cnt) = d_data(i,j,k,l);
+            }
+          }
+        }
+      }
+    }
+  };
+};
+
+template <typename Device, typename T>
+class SparseTensorDenseConv3D : public OpKernel {
+ public:
+  explicit SparseTensorDenseConv3D(OpKernelConstruction* context) : OpKernel(context) {
+    OP_REQUIRES_OK(context, context->GetAttr("strides", &stride_));
+    OP_REQUIRES(context, stride_.size() == 5,
+                errors::InvalidArgument("Sliding window strides field must "
+                                        "specify 5 dimensions"));
   }
 
-  inline void TTypes<Index>::Matrix indices dense_filter_update_output(TTypes<Index>::Matrix& a_conv_filter, TTypes<Index>::Matrix& a_input_indices, ){
+  void Compute(OpKernelContext* context) override {
 
+    //get input data
+    const Tensor *in_indices, *in_values, *in_shape, *dense_filter;
+    OP_REQUIRES_OK(context, context->input("in_indices", &in_indices));
+    OP_REQUIRES_OK(context, context->input("in_values", &in_values));
+    OP_REQUIRES_OK(context, context->input("in_shape", &in_shape));
+    OP_REQUIRES_OK(context, context->input("dense_filter", &dense_filter));
+    auto in_ind = in_indices->matrix<int64>(); //channels, depth, height, width, optionally others TODO: other cases?
+    auto in_vals = in_values->flat<T>();
+    auto in_sh = in_shape->flat<int64>();
+    std::map<std::vector<int64>, T> output_map; //stores the values for the output tensor
+    Tensor *filter_indices = NULL, *filter_values = NULL, *filter_shape = NULL;
+    dense_to_sparse(dense_filter, filter_indices, filter_values, filter_shape);
+    auto f_ind = filter_indices->matrix<int64>(); //filters, channels, kernel_depth, kernel_height, kernel_width TODO: other cases?
+    auto f_vals = filter_values->flat<T>();
+    auto f_sh = filter_shape->flat<int64>();
+    sparseCuboidConv3D(in_ind, in_vals, in_sh, f_ind, f_vals, f_sh, stride_, output_map);
+
+
+    // Create an output tensor
+    Tensor *sparse_values = NULL, *sparse_indices = NULL, *sparse_shape = NULL;
+    TensorShape out_ind_shape = {(int64) output_map.size(), (int64) in_ind.dimension(1)};
+    TensorShape out_val_shape = {(int64) output_map.size()};
+    TensorShape out_sh_shape = {(int64) in_ind.dimension(1)};
+    OP_REQUIRES_OK(context, context->allocate_output("sparse_indices", out_ind_shape, &sparse_indices));
+    OP_REQUIRES_OK(context, context->allocate_output("sparse_values", out_val_shape, &sparse_values));
+    OP_REQUIRES_OK(context, context->allocate_output("sparse_shape", out_sh_shape, &sparse_shape));
+
+    auto out_ind = sparse_indices->matrix<int64>(); //channels, depth, height, width, optionally others TODO: other cases?
+    auto out_vals = sparse_values->flat<T>();
+    auto out_sh = sparse_shape->flat<int64>();
+
+    int64 idx = 0;
+    for(auto it = output_map.begin(); it != output_map.end(); ++it, idx++){
+        const std::vector<int64> &indice = it->first;
+        for(int64 j = 0; j < indice.size(); ++j){
+          out_ind(idx,j) = indice[j];
+        }
+        out_vals(idx) = it->second;
+    }
+    for(int64 idx = 0; idx < in_ind.dimension(1); ++idx){
+        if(idx == 0){
+          out_sh(idx) = f_sh(idx); //output number of channels == number of filters
+        } else {
+          out_sh(idx) = in_sh(idx);
+        }
+    }
+    delete filter_indices, filter_values, filter_shape;
   }
-  */
+
+  inline void
+  dense_to_sparse(const Tensor* d_tensor, Tensor* s_indices, Tensor* s_values, Tensor* s_shape, const T epsilon = 0) const {
+    std::map<std::vector<int64>, T> sparse_map;
+    int64 dims = d_tensor->dims();
+
+    auto d_flat = d_tensor->flat<T>();
+
+    int64 non_zero_cnt = 0;
+    for(int64 i = 0;  i < d_flat.size(); ++i){
+      if(d_flat(i) > epsilon || d_flat(i) < epsilon) non_zero_cnt++;
+    }
+
+    delete(s_indices); delete(s_shape); delete(s_values);
+    TensorShape out_ind_shape = {(int64) non_zero_cnt, dims};
+    TensorShape out_val_shape = {(int64) non_zero_cnt};
+    TensorShape out_sh_shape = {dims};
+    s_indices = new Tensor(DT_INT64, out_ind_shape);
+    s_shape = new Tensor(DT_INT64, out_sh_shape);
+    s_values = new Tensor(d_tensor->dtype(), out_sh_shape);
+    auto out_sh = s_shape->flat<int64>();
+    for(size_t i = 0; i < out_sh.size(); ++i){
+      out_sh(i) = d_tensor->dim_size(i);
+    }
+
+
+    switch (dims) {
+#define NDIMS_CASE(N)                                                               \
+  case N: {                                                                         \
+    Converter<T, N> c;                                                              \
+    c.convert(d_tensor, s_indices, s_values, epsilon);                              \
+  } break;
+
+      NDIMS_CASE(4);
+      NDIMS_CASE(5);
+#undef NDIMS_CASE
+    }
+  }
+
+ private:
+  std::vector<int32> stride_;
 };
 
 
-REGISTER_KERNEL_BUILDER(Name("SparseTensorDenseConv3D").Device(DEVICE_CPU), SparseTensorDenseConv3D);
+
+
+#define REGISTER_CPU(type)                                   \
+  REGISTER_KERNEL_BUILDER(Name("SparseTensorDenseConv3D").Device(DEVICE_CPU), SparseTensorDenseConv3D<CPUDevice, type>);
+
+//REGISTER_CPU(float);
+//REGISTER_CPU(double);
+REGISTER_CPU(int32);
+//REGISTER_CPU(complex64);
+//REGISTER_CPU(complex128);
--- tensorflow/core/user_ops/sparse_tensor_sparse_kernel_dense_conv_3d.cc
+++ tensorflow/core/user_ops/sparse_tensor_sparse_kernel_dense_conv_3d.cc
@@ -3,6 +3,7 @@
 #include "tensorflow/core/framework/op.h"
 #include "tensorflow/core/framework/shape_inference.h"
 #include "tensorflow/core/framework/common_shape_fns.h"
+#include "sparse_tensor_sparse_kernel_dense_conv_3d.h"
 
 
 /** SparseTensorSparseKernelDenseConv3D
@@ -73,53 +74,7 @@ class SparseTensorSparseKernelDenseConv3D : public OpKernel {
 
     std::map<std::vector<int64>, T> output_map; //stores the values for the output tensor
 
-    //preparation: find center of filter
-    std::vector<int64> filter_offset(f_ind.dimension(1));
-    for(int64 i = 0; i < filter_offset.size(); ++i){
-      filter_offset[i] = (f_sh(i) - 1) / 2; //TODO: precompute filter indices with offset? 
-    }
-
-    for(int64 i = 0; i < in_ind.dimension(0); ++i){ //TODO: parallelize filtering
-      if(!index_is_unaffected_by_stride(in_ind, stride_, i)) continue; // go through all indices of input and check if valid (not affected by stride)
-
-      //a) prepare filter to update output based on current value
-      std::map<std::vector<int64>, T> filter_update;
-      std::vector<int64> iids(in_ind.dimension(1));
-      for(int64 j = 0; j < in_ind.dimension(1); j++){
-        iids[j] = in_ind(i,j);
-      }
-      auto &input_value = in_vals(i);
-      for(int64 j = 0; j < f_ind.dimension(0); j++){
-        if(f_ind(j, 1) != in_ind(i,0)) continue; //filter channel != input channel
-        bool is_in_bound = true;
-        std::vector<int64> update_ids(in_ind.dimension(1), 0);
-        assert(update_ids.size() >= f_ind.dimension(1) - 1);
-        update_ids[0] = f_ind(j,0); //output channel is filter number
-        for(int64 k = 2; k < f_ind.dimension(1); k++){ //TODO: ugly coding style... prototype
-          update_ids[k - 1] = iids[k - 1] + f_ind(j,k) - filter_offset[k];  //depth, width and height
-          if(update_ids[k - 1] < 0 || update_ids[k - 1] >= in_sh(k-1)){ //check boundaries
-            is_in_bound = false;
-            break;
-          }
-        }
-        if(is_in_bound){
-          T update_val = input_value * f_vals(j); //input value times filter weight at index
-          filter_update.insert(std::make_pair(update_ids, update_val));
-        }
-      }
-      
-
-      //b) check if output exists (search required) and create or update output based on filter_update
-        //TODO: concept: tree search to find upper and lower filter bound; binary search in between?
-      for(auto it = filter_update.begin(); it != filter_update.end(); ++it){
-        auto res_it = output_map.find(it->first);
-        if(res_it == output_map.end()){
-          output_map.insert(*it);
-        } else {
-          res_it->second += it->second;
-        }
-      }
-    }
+    sparseCuboidConv3D(in_ind, in_vals, in_sh, f_ind, f_vals, f_sh, stride_, output_map);
 
     // Create an output tensor
     Tensor *sparse_values = NULL, *sparse_indices = NULL, *sparse_shape = NULL;
@@ -163,19 +118,6 @@ class SparseTensorSparseKernelDenseConv3D : public OpKernel {
   }
 
  private:
-  template<typename Matrix2DT>  
-  inline bool index_is_unaffected_by_stride(const Matrix2DT& ids, const std::vector<int32>& stride, const int& a_id) const {
-    //assert(id.size() == stride.size());
-    for(int32 i = 0; i < stride.size(); ++i){
-      if(stride[i] > 1){
-        if(((ids(a_id,i) + 1) % stride[i]) == 0){
-          return false;
-        }
-      }
-    }
-    return true;
-  }
-
   std::vector<int32> stride_;
 };
 
