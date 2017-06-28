ac0551d4ac322e7a9cb3f1d022865d3245eb2b5d
Timo Hackel <timo.hackel@geod.baug.ethz.ch>
2017-06-28 16:27:27 +0200

    started to work on gpu implementation

--- tensorflow/core/user_ops/tests/eval_conv_layer.m
+++ tensorflow/core/user_ops/tests/eval_conv_layer.m
@@ -2,36 +2,54 @@ clear;
 clc;
 close all;
 
-data = importdata('eval_time_conv_2.txt');
+% data = importdata('eval_time_conv.txt');
+% 
+% res = data(:,1)';
+% dc = data(:,2)';
+% sc = data(:,3)';
+% asc = data(:,4)';
+% 
+% dbpf = data(:,5)';
+% dbpi = data(:,6)';
+% sbpf = data(:,7)';
+% sbpi = data(:,8)';
+% sbpc = data(:,9)';
+% 
+% 
+% hold on;
+% 
+% hTitle  = title ('Runtime on CPU with varying resolution');
+% hXLabel = xlabel('resolution : density = 1 / resolution', 'FontSize',13);
+% hYLabel = ylabel('Time [s]', 'FontSize',13);
+% plot(res, dc, 'c','LineWidth',2);
+% plot(res, sc, 'b','LineWidth',2);
+% %hLegend = legend('dense conv', 'sparse conv')
+% plot(res(:,1:33), dbpf(:, 1:33), 'g','LineWidth',2);
+% plot(res, sbpf, 'r','LineWidth',2);
+% plot(res(:,1:33), dbpi(:,1:33), 'k','LineWidth',2);
+% plot(res, sbpi, 'm','LineWidth',2);
+% plot(res, sbpc,  'color', [0.2 0.2 0.6],'LineWidth',2);
+% plot(res, asc,'color', [0.5 0.5 0.5], 'LineWidth',2);
+% 
+% hLegend = legend('dense conv', 'sparse conv', 'dense bp filter', 'sparse bp filter', 'dense bp input', 'sparse bp input','direct sparse backprop','approx conv', 'Location', 'northwest')
+
+data = importdata('eval_time_relu.txt');
 
 res = data(:,1)';
 dc = data(:,2)';
 sc = data(:,3)';
-asc = data(:,4)';
 
-dbpf = data(:,5)';
-dbpi = data(:,6)';
-sbpf = data(:,7)';
-sbpi = data(:,8)';
-sbpc = data(:,9)';
 
 
 hold on;
 
-hTitle  = title ('Runtime on CPU with varying resolution');
+hTitle  = title ('ReLU on CPU with varying resolution');
 hXLabel = xlabel('resolution : density = 1 / resolution', 'FontSize',13);
 hYLabel = ylabel('Time [s]', 'FontSize',13);
 plot(res, dc, 'c','LineWidth',2);
 plot(res, sc, 'b','LineWidth',2);
-%hLegend = legend('dense conv', 'sparse conv')
-plot(res(:,1:33), dbpf(:, 1:33), 'g','LineWidth',2);
-plot(res, sbpf, 'r','LineWidth',2);
-plot(res(:,1:33), dbpi(:,1:33), 'k','LineWidth',2);
-plot(res, sbpi, 'm','LineWidth',2);
-plot(res, sbpc, 'x','LineWidth',2);
-plot(res, asc, '.','LineWidth',2);
 
-hLegend = legend('dense conv', 'sparse conv', 'dense bp filter', 'sparse bp filter', 'dense bp input', 'sparse bp input','direct sparse backprop','approx conv', 'Location', 'northwest')
+hLegend = legend('dense relu', 'sparse relu', 'Location', 'northwest')
 
 
 set( gca                       , ...
