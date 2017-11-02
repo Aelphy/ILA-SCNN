bug fix: buffer overflow and output shape

tensorflow/core/user_ops/direct_sparse_experiments/modelnet/modelnet_models.py
@@ -33,7 +33,7 @@ def model_modelnet10_8(sparse_data, tensor_in_sizes, train_labels = None, num_cl
   sd_converted = ld.create_sparse_data_to_direct_sparse(sparse_data, dim)
   sc1 = ld.create_sparse_conv_layer(sd_converted, [3,3,3,1,8], strides, padding, dim, 0.8, "K-ABS", name = scope + "sc1", initializer=initializer, regularizer=regularizer)
   sd = ld.create_direct_sparse_to_dense(sc1, dim)
-  sd_flat = tf.reshape(sd, [batch_size, total_size])
+  sd_flat = tf.reshape(sd, [batch_size, total_size * 8])
   if train_labels != None:
     conv_out = tf.nn.dropout(sd_flat, 0.5, name="dropout")
   else:
@@ -61,18 +61,18 @@ def model_modelnet10_256(sparse_data, tensor_in_sizes, train_labels = None, num_
   sd_converted = ld.create_sparse_data_to_direct_sparse(sparse_data, dim)
   d1 = 0.01
   net = ld.create_sparse_conv_layer(sd_converted, [3,3,3,1,8], strides, padding, dim, d1, "K-RELU", name = scope + "sc1", initializer=initializer, regularizer=regularizer)
-  #net = ld.create_sparse_conv_layer(net, [3,3,3,8,8], strides, padding, dim, d1, "K-RELU", name = scope + "sc2", initializer=initializer, regularizer=regularizer)
-  #net = ld.create_sparse_conv_layer(net, [3,3,3,8,8], strides, padding, dim, d1, "K-RELU", name = scope + "sc3", initializer=initializer, regularizer=regularizer)
+  net = ld.create_sparse_conv_layer(net, [3,3,3,8,8], strides, padding, dim, d1, "K-RELU", name = scope + "sc2", initializer=initializer, regularizer=regularizer)
+  net = ld.create_sparse_conv_layer(net, [3,3,3,8,8], strides, padding, dim, d1, "K-RELU", name = scope + "sc3", initializer=initializer, regularizer=regularizer)
   net = ld.create_sparse_pooling_layer(net, pooling_sizes, dim)
   d2 = 0.03
-  #net = ld.create_sparse_conv_layer(net, [3,3,3,8,16], strides, padding, dim, d2, "K-RELU", name = scope + "sc4", initializer=initializer, regularizer=regularizer)
-  #net = ld.create_sparse_conv_layer(net, [3,3,3,16,16], strides, padding, dim, d2, "K-RELU", name = scope + "sc5", initializer=initializer, regularizer=regularizer)
-  #net = ld.create_sparse_conv_layer(net, [3,3,3,16,16], strides, padding, dim, d2, "K-RELU", name = scope + "sc6", initializer=initializer, regularizer=regularizer)
+  net = ld.create_sparse_conv_layer(net, [3,3,3,8,16], strides, padding, dim, d2, "K-RELU", name = scope + "sc4", initializer=initializer, regularizer=regularizer)
+  net = ld.create_sparse_conv_layer(net, [3,3,3,16,16], strides, padding, dim, d2, "K-RELU", name = scope + "sc5", initializer=initializer, regularizer=regularizer)
+  net = ld.create_sparse_conv_layer(net, [3,3,3,16,16], strides, padding, dim, d2, "K-RELU", name = scope + "sc6", initializer=initializer, regularizer=regularizer)
   net = ld.create_sparse_pooling_layer(net, pooling_sizes, dim)
   d3 = 0.05
-  #net = ld.create_sparse_conv_layer(net, [3,3,3,16,24], strides, padding, dim, d3, "K-RELU", name = scope + "sc7", initializer=initializer, regularizer=regularizer)
-  #net = ld.create_sparse_conv_layer(net, [3,3,3,24,24], strides, padding, dim, d3, "K-RELU", name = scope + "sc8", initializer=initializer, regularizer=regularizer)
-  #net = ld.create_sparse_conv_layer(net, [3,3,3,24,24], strides, padding, dim, d3, "K-RELU", name = scope + "sc9", initializer=initializer, regularizer=regularizer)
+  net = ld.create_sparse_conv_layer(net, [3,3,3,16,24], strides, padding, dim, d3, "K-RELU", name = scope + "sc7", initializer=initializer, regularizer=regularizer)
+  net = ld.create_sparse_conv_layer(net, [3,3,3,24,24], strides, padding, dim, d3, "K-RELU", name = scope + "sc8", initializer=initializer, regularizer=regularizer)
+  net = ld.create_sparse_conv_layer(net, [3,3,3,24,24], strides, padding, dim, d3, "K-RELU", name = scope + "sc9", initializer=initializer, regularizer=regularizer)
   net = ld.create_sparse_pooling_layer(net, pooling_sizes, dim)
   net = ld.create_direct_sparse_to_dense(net, dim)
   net = tf.reshape(net, [batch_size, 32, 32, 32, 24])

tensorflow/core/user_ops/direct_sparse_experiments/modelnet/train_modelnet.py
@@ -76,13 +76,14 @@ random_sparse_data = tf.SparseTensor(indices=data_ind, values=data_val, dense_sh
 [label_ind, label_val, label_sh] = sp.createRandomSparseTensor(1, batch_label_sizes)
 random_dense_label = sp.sparse_to_dense(label_ind, label_val, label_sh)
 with tf.Session(config=config) as sess:
-  sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+  #sess = tf_debug.LocalCLIDebugWrapperSession(sess)
   print("writing graph")
   writer = tf.summary.FileWriter("/tmp/test", sess.graph)
   trainable = tf.trainable_variables()
   print("trainable: ", trainable)
   feed_dict={sparse_data: tf.SparseTensorValue(data_ind, data_val, data_sh), dense_labels: random_dense_label}
   sess.run(initall, feed_dict=feed_dict)
+  print("data initialized")
   if len(pretrained_model) > 0:
     saver.restore(sess,pretrained_model)
   for epoch in range(1, max_epochs):
@@ -99,6 +100,7 @@ with tf.Session(config=config) as sess:
       reader.start()
       values_ = np.array(batch[1], dtype=np.float32)
       indices_ = np.array(batch[0], dtype =np.int64)
+      print(indices_, values_, batch[2])
       feed_dict={sparse_data: tf.SparseTensorValue(indices_, values_, batch[2]), dense_labels: batch[3]}
       t_train1 = time.time()
       #perform training