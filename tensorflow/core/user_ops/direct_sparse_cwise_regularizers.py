9a97fc343ad6051804cc107a551ca64329b3afb5
Timo Hackel <timo.hackel@geod.baug.ethz.ch>
2017-12-05 11:52:55 +0100

    started to work on channelwise regularizers

---
+++ tensorflow/core/user_ops/direct_sparse_cwise_regularizers.py
@@ -0,0 +1,45 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numbers
+
+from tensorflow.python.framework import constant_op
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import nn
+from tensorflow.python.ops import standard_ops
+from tensorflow.python.platform import tf_logging as logging
+from direct_sparse_module import sparse_nn_ops as sc_module
+
+def biased_l2_regularizer(scale, bias, indices, shape, fc_mapping, scope=None):
+  """Returns a function that can be used to apply L2 regularization to weights.
+  Small values of L2 can help prevent overfitting the training data.
+  Args:
+    scale: A scalar multiplier `Tensor`. 0.0 disables the regularizer.
+    scope: An optional scope name.
+  Returns:
+    A function with signature `l2(weights)` that applies L2 regularization.
+  Raises:
+    ValueError: If scale is negative or if scale is not a float.
+  """
+  if isinstance(scale, numbers.Integral):
+    raise ValueError('scale cannot be an integer: %s' % (scale,))
+  if isinstance(scale, numbers.Real):
+    if scale < 0.:
+      raise ValueError('Setting a scale less than 0 on a regularizer: %g.' %
+                       scale)
+    if scale == 0.:
+      logging.info('Scale of 0 disables regularizer.')
+      return lambda _: None
+  
+  def l2(weights):
+    """Applies l2 regularization to weights."""
+    with ops.name_scope(scope, 'l2_regularizer', [weights]) as name:
+      my_scale = ops.convert_to_tensor(scale,
+                                       dtype=weights.dtype.base_dtype,
+                                       name='scale')
+
+      return tf.reduce_sum(sc_module.direct_sparse_cwise_biased_l2_regularization(indices, weights, shape, fc_mapping, bias, my_scale)) #TODO: get rid of reduced sum and apply reg. loss per channel
+
+  return l2
